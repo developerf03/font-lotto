becb6079df7ff544f2e83b47915c22464ce97a8f
nottdark <nottdark.dev@gmail.com>
2024-11-03 04:41:03 +0700

    Fix plugin init

--- plugins/01-init.js
+++ plugins/01-init.js
@@ -3,17 +3,19 @@ export default defineNuxtPlugin(async ({ $pinia }) => {
   const { getHeaderInfo, getSetting } = useSettingStore($pinia)
   const { setting } = storeToRefs(useSettingStore($pinia))
 
-  try {
-    if (settingCookie.value) {
-      setting.value = settingCookie.value
-      Promise.all([getHeaderInfo(), getSetting()])
-    } else {
-      const [setting] = await Promise.all([getSetting(), getHeaderInfo()])
+  if (import.meta.server) {
+    try {
+      if (settingCookie.value) {
+        setting.value = settingCookie.value
+        Promise.all([getHeaderInfo(), getSetting()])
+      } else {
+        const [setting] = await Promise.all([getSetting(), getHeaderInfo()])
 
-      settingCookie.value = setting
+        settingCookie.value = setting
+      }
+      // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      // 
     }
-  // eslint-disable-next-line no-unused-vars
-  } catch (error) {
-    // 
   }
 })

6c4ada935b91f1dfb8c5bdee699bf4d180483c0e
nottdark <nottdark.dev@gmail.com>
2024-11-03 03:37:48 +0700

    - Add ignore api path send Authorization - Add setting into cookie

--- plugins/01-init.js
+++ plugins/01-init.js
@@ -1,5 +1,19 @@
 export default defineNuxtPlugin(async ({ $pinia }) => {
+  const settingCookie = useCookie('setting')
   const { getHeaderInfo, getSetting } = useSettingStore($pinia)
+  const { setting } = storeToRefs(useSettingStore($pinia))
 
-  await Promise.all([getHeaderInfo(), getSetting()])
+  try {
+    if (settingCookie.value) {
+      setting.value = settingCookie.value
+      Promise.all([getHeaderInfo(), getSetting()])
+    } else {
+      const [setting] = await Promise.all([getSetting(), getHeaderInfo()])
+
+      settingCookie.value = setting
+    }
+  // eslint-disable-next-line no-unused-vars
+  } catch (error) {
+    // 
+  }
 })
